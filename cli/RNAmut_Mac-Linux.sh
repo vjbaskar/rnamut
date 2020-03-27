606059f4b37a9eeedd7af930653a5f6bc062df49
Vijay <vjbaskar@gmail.com>
2020-03-27 19:09:32 +0000

    Added memory support in command line

--- cli/RNAmut_Mac-Linux.sh
+++ cli/RNAmut_Mac-Linux.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
+Help() {
+    echo "
+    You should provide a minimal option eg.
+    RNAmut <memory to be used (mb)>
+    eg> RNAmut 2048
+    "
+}
+
+
+if [ $# -eq 0 ]; then
+    Help
+    exit 0
+fi
+
+mem_in_mb=$1; shift
+i=${mem_in_mb}
+if  test -z "${i//[0-9]}" ; then
+    continue
+else
+    Help    
+    exit 1
+fi
 JAVA_COMMAND="java"
-JAVA_OPTIONS="-Xms1g -Xmx1g"
+JAVA_OPTIONS="-Xms${mem_in_mb}m -Xmx${mem_in_mb}m"
 
 #%JAR_FILE="/home/vijay/tmpd/rnamut/RNAmut.jar"
 
